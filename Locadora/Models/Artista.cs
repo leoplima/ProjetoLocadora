805aded0fc4c9029d6c7a3e61ee86033c9da7f37
Leonardo <leo.plima08@gmail.com>
2017-05-09 15:57:32 -0300

    PrimeiroCommit

---
+++ Locadora/Models/Artista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public  class Artista
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNasc { get; set; }
+        public string Pais { get; set; }
+        public string Foto { get; set; }
+    }
+}
