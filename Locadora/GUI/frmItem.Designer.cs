c46e6b23345bd93e8a8a369e59a53541073443c0
Leonardo <leo.plima08@gmail.com>
2017-05-09 19:48:28 -0300

    ClasseItem

--- Locadora/GUI/frmItem.Designer.cs
+++ Locadora/GUI/frmItem.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(448, 377);
             this.Name = "frmItem";
             this.Text = "Cadastro Filmes & Séries";
             this.ResumeLayout(false);
--- Locadora/Models/Item.cs
+++ Locadora/Models/Item.cs
@@ -9,5 +9,19 @@ namespace Models
     public class Item
     {
         public int Codigo { get; set; }
+        public string CodBarras { get; set; }
+        public int CodCategoria { get; set; }
+        public string Nome { get; set; }
+        public int CodGenero { get; set; }
+        public string Ano { get; set; }
+        public int CodTipo { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime Data { get; set; }
+        public decimal VrCusto { get; set; }
+        public int CodSituacao { get; set; }
+        public int CodArtista { get; set; }
+        public string Diretor { get; set; }
+        public string Foto { get; set; }
+
     }
 }
